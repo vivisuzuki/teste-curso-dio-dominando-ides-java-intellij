597fb438077570a2d25227ddd11a6e5538b6e846
vivisuzuki <vivisuzuki@hotmail.com>
2022-03-01 11:11:15 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,8 +9,46 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Fahreneit 451",216);
+
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer nPag;
+
+    public Livro(String nome, Integer nPag) {
+        this.nome = nome;
+        this.nPag = nPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getnPag() {
+        return nPag;
+    }
+
+    public void setnPag(Integer nPag) {
+        this.nPag = nPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nPag=" + nPag +
+                '}';
     }
 }
